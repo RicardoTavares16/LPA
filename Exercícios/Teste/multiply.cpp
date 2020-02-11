529166a607b2bfaddd71dd44383051dc635ac0d8
Ricardo Tavares <ricardotavares16@gmail.com>
2020-02-11 19:28:58 +0000

    test

---
+++ Exercícios/Teste/multiply.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]){
+int mult1, mult2, answer = 0;
+cout << "Type two integers \n";
+cin >> mult1;
+cin >> mult2;
+answer = mult1 * mult2;
+cout << answer << "\n";
+return 0;
+}
