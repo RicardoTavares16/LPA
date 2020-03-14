14cda961790c67084712080e6a08ab59f4689fa0
Ricardo Tavares <ricardotavares16@gmail.com>
2020-03-14 18:40:32 +0000

    test exercise

---
+++ Exercícios/parentheses/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+// ( -> 40 | ) -> 41 | [ -> 91 | ] -> 93
+
+
+
+
+int main( ) {
+
+std::string input_string;
+
+	std::getline(std::cin, input_string);
+	while(std::getline(std::cin, input_string)) {
+
+	};
+
+	return 0;
+}
