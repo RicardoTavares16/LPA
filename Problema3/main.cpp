9ebe974ce2447b182d77a0fbd10b4eae8bee5710
Ricardo Tavares <ricardotavares16@gmail.com>
2020-06-12 17:38:31 +0100

    Floyd warshall test

--- Problema3/main.cpp
+++ Problema3/main.cpp
@@ -2,12 +2,37 @@
 #include <cstring>
 #include <cstdlib>
 #include <vector>
+#include <limits>
+
 
 #define MAX 1000
+#define for11(it, res) for(int it = 1; it <= res; it++)
+#define INF std::numeric_limits<int>::max() / 2;
+
 std::vector< std::vector< std::pair<int,int> > > matrix;
 
 int nVertex, apCount, dfs[MAX], low[MAX], parent[MAX], dfsTimer;
 bool isAP[MAX];
+int APs[MAX];
+
+int **dist;
+
+void alloc() {
+    dist = (int**) malloc((nVertex + 1) * sizeof(int*));
+    for11(i, nVertex) {
+        dist[i] = (int*) malloc((nVertex + 1) * sizeof(int));
+    }
+        //printf("ALLOC INPUT %d\n", nVertex);
+
+}
+
+void unAlloc() {
+    for11(i, nVertex) {
+        free(dist[i]);
+    }
+    free(dist);
+    //printf("UNALLOC INPUT %d\n", nVertex);
+}
 
 void resetData()
 {
@@ -20,6 +45,7 @@ void resetData()
         dfs[i] = -1;
         isAP[i] = false;
     }
+    alloc();
 }
 
 void articulationPoints(int v)
@@ -108,9 +134,45 @@ void readInput()
         //Make adjancy matrix with costs
         matrix[vertexA].push_back(std::make_pair(vertexB,cost));
         matrix[vertexB].push_back(std::make_pair(vertexA,cost));
+
+        dist[vertexA][vertexB] = cost;
+
+        //printf("%d %d cost: %d\n", vertexA, vertexB, cost);
+        //printf("VertexA %d first: %d second: %d\n", vertexA, matrix[vertexA][vertexB].first, matrix[vertexA][vertexB].second);
     }
 }
 
+void floydWarshall() {
+    int i, j, k;
+
+    for (i = 1; i <= nVertex; i++){
+        for (j = 1; j <= nVertex; j++)
+        {
+            if(dist[i][j])
+                dist[i][j] = dist[i][j];
+            else if(i == j){
+                dist[i][j] = 0;
+            }
+            else
+                dist[i][j] = INF;
+        }
+    }
+
+    for (k = 1; k <= nVertex; k++) {
+        for (i = 1; i <= nVertex; i++) {
+            for (j = 1; j <= nVertex; j++) {
+                dist[i][j] = std::min(dist[i][j],dist[i][k]+dist[k][j]);
+            }
+        }
+    }
+}
+
+void readQueries() {
+    int s, t;
+    while(std::cin >> s >> t) {
+        std::cout << dist[s][t] << std::endl;
+    }
+}
 int main()
 {
     int input;
@@ -130,11 +192,19 @@ int main()
 
         //Number of servers
         int serverNumber = articulationCount();
+       
 
         if(serverNumber == 0){
             printf("no server\n");
+            //   for (int i = 1; i <= nVertex; i++)
+            // {
+            //     for (int j = 1; j <= nVertex; j++)
+            //     {
+            //         printf("%d %d cost: %d\n", i, j, dist[i][j]);
+            //     }
+            // }
         }
-        else{
+        else if(serverNumber == 1){
             printf("%d %d %d\n", serverNumber, 0, 0);
             
             #ifdef DEBUG
@@ -142,7 +212,46 @@ int main()
                 printf("Vertex %d: AP: %d \n", i, isAP[i]);
             }
             #endif
+            //  for (int i = 1; i <= nVertex; i++)
+            // {
+            //     for (int j = 1; j <= nVertex; j++)
+            //     {
+            //         printf("%d %d cost: %d\n", i, j, dist[i][j]);
+            //     }
+            // }
         }
+        else{
+              
+
+            floydWarshall();
+            int linkServersCost = 0;
+            
+            for(int i = 1; i <= nVertex; i++){
+                if(isAP[i]){
+                    for(int j = i; j <= nVertex-i; j++){
+                        if(isAP[j]){
+                            if(dist[i][j] != std::numeric_limits<int>::max() / 2){
+                                linkServersCost += dist[i][j];
+                            }
+                        }
+                    }
+                }
+            }
+            //  for (int i = 1; i <= nVertex; i++)
+            // {
+            //     for (int j = 1; j <= nVertex; j++)
+            //     {
+            //         if(dist[i][j] != 0 && dist[i][j] != std::numeric_limits<int>::max() / 2){
+            //         printf("%d %d cost: %d\n", i, j, dist[i][j]);
+            //         }
+            //     }
+            // }
+
+            printf("%d %d %d\n", serverNumber, linkServersCost, 0);
+         
+        }
+        //readQueries();
+        unAlloc();
     }
 
     return 0;
