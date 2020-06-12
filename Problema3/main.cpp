0f83f6a26bff7c2ab136ea5d41f256aa8846a305
Ricardo Tavares <ricardotavares16@gmail.com>
2020-06-12 17:47:21 +0100

    .

--- Problema3/main.cpp
+++ Problema3/main.cpp
@@ -4,30 +4,18 @@
 #include <vector>
 #include <limits>
 
-
 #define MAX 1000
-#define for11(it, res) for(int it = 1; it <= res; it++)
-#define INF std::numeric_limits<int>::max() / 2;
 
-std::vector< std::vector< std::pair<int,int> > > matrix;
+std::vector<std::vector<std::pair<int, int>>> matrix;
 
 int nVertex, apCount, dfs[MAX], low[MAX], parent[MAX], dfsTimer;
 bool isAP[MAX];
-int APs[MAX];
-
 int **dist;
 
-void alloc() {
-    dist = (int**) malloc((nVertex + 1) * sizeof(int*));
-    for11(i, nVertex) {
-        dist[i] = (int*) malloc((nVertex + 1) * sizeof(int));
-    }
-        //printf("ALLOC INPUT %d\n", nVertex);
-
-}
-
-void unAlloc() {
-    for11(i, nVertex) {
+void freeArray()
+{
+    for (int i = 1; i <= nVertex; i++)
+    {
         free(dist[i]);
     }
     free(dist);
@@ -40,12 +28,18 @@ void resetData()
     matrix.resize(nVertex + 1);
     apCount = 0;
     dfsTimer = 0;
+
     for (int i = 1; i <= nVertex; i++)
     {
         dfs[i] = -1;
         isAP[i] = false;
     }
-    alloc();
+
+    dist = (int **)malloc((nVertex + 1) * sizeof(int *));
+    for (int i = 1; i <= nVertex; i++)
+    {
+        dist[i] = (int *)malloc((nVertex + 1) * sizeof(int));
+    }
 }
 
 void articulationPoints(int v)
@@ -109,11 +103,11 @@ void readInput()
         strcpy(cstr, str.c_str());
         char *token;
 
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("String: %s\n", cstr);
-        #endif
+#endif
 
-        //Read first int    
+        //Read first int
         token = strtok(cstr, " ");
 
         int vertexA = atoi(token);
@@ -128,12 +122,12 @@ void readInput()
         token = strtok(NULL, " ");
         int cost = atoi(token);
 
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("%d %d %d\n", vertexA, vertexB, cost);
-        #endif
+#endif
         //Make adjancy matrix with costs
-        matrix[vertexA].push_back(std::make_pair(vertexB,cost));
-        matrix[vertexB].push_back(std::make_pair(vertexA,cost));
+        matrix[vertexA].push_back(std::make_pair(vertexB, cost));
+        matrix[vertexB].push_back(std::make_pair(vertexA, cost));
 
         dist[vertexA][vertexB] = cost;
 
@@ -142,37 +136,37 @@ void readInput()
     }
 }
 
-void floydWarshall() {
+void floydWarshall()
+{
     int i, j, k;
 
-    for (i = 1; i <= nVertex; i++){
+    for (i = 1; i <= nVertex; i++)
+    {
         for (j = 1; j <= nVertex; j++)
         {
-            if(dist[i][j])
+            if (dist[i][j])
                 dist[i][j] = dist[i][j];
-            else if(i == j){
+            else if (i == j)
+            {
                 dist[i][j] = 0;
             }
             else
-                dist[i][j] = INF;
+                dist[i][j] = std::numeric_limits<int>::max() / 2;
         }
     }
 
-    for (k = 1; k <= nVertex; k++) {
-        for (i = 1; i <= nVertex; i++) {
-            for (j = 1; j <= nVertex; j++) {
-                dist[i][j] = std::min(dist[i][j],dist[i][k]+dist[k][j]);
+    for (k = 1; k <= nVertex; k++)
+    {
+        for (i = 1; i <= nVertex; i++)
+        {
+            for (j = 1; j <= nVertex; j++)
+            {
+                dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
     }
 }
 
-void readQueries() {
-    int s, t;
-    while(std::cin >> s >> t) {
-        std::cout << dist[s][t] << std::endl;
-    }
-}
 int main()
 {
     int input;
@@ -183,18 +177,18 @@ int main()
         input = std::atoi(str.c_str());
         nVertex = input;
 
-        #ifdef DEBUG
+#ifdef DEBUG
         printf("Input: %d\n", input);
-        #endif
+#endif
 
         resetData(); //Clear matrix, reset counters
         readInput(); //Start reading input
 
         //Number of servers
         int serverNumber = articulationCount();
-       
 
-        if(serverNumber == 0){
+        if (serverNumber == 0)
+        {
             printf("no server\n");
             //   for (int i = 1; i <= nVertex; i++)
             // {
@@ -204,14 +198,16 @@ int main()
             //     }
             // }
         }
-        else if(serverNumber == 1){
+        else if (serverNumber == 1)
+        {
             printf("%d %d %d\n", serverNumber, 0, 0);
-            
-            #ifdef DEBUG
-            for(int i = 0; i < nVertex; i++){
+
+#ifdef DEBUG
+            for (int i = 0; i < nVertex; i++)
+            {
                 printf("Vertex %d: AP: %d \n", i, isAP[i]);
             }
-            #endif
+#endif
             //  for (int i = 1; i <= nVertex; i++)
             // {
             //     for (int j = 1; j <= nVertex; j++)
@@ -220,17 +216,22 @@ int main()
             //     }
             // }
         }
-        else{
-              
+        else
+        {
 
             floydWarshall();
             int linkServersCost = 0;
-            
-            for(int i = 1; i <= nVertex; i++){
-                if(isAP[i]){
-                    for(int j = i; j <= nVertex-i; j++){
-                        if(isAP[j]){
-                            if(dist[i][j] != std::numeric_limits<int>::max() / 2){
+
+            for (int i = 1; i <= nVertex; i++)
+            {
+                if (isAP[i])
+                {
+                    for (int j = i; j <= nVertex - i; j++)
+                    {
+                        if (isAP[j])
+                        {
+                            if (dist[i][j] != std::numeric_limits<int>::max() / 2)
+                            {
                                 linkServersCost += dist[i][j];
                             }
                         }
@@ -248,10 +249,8 @@ int main()
             // }
 
             printf("%d %d %d\n", serverNumber, linkServersCost, 0);
-         
         }
-        //readQueries();
-        unAlloc();
+        freeArray();
     }
 
     return 0;
