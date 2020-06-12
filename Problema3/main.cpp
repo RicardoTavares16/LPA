fe0cef162af05d519d535edf03e103ba8d73dc97
Ricardo Tavares <ricardotavares16@gmail.com>
2020-06-12 21:20:54 +0100

    .

--- Problema3/main.cpp
+++ Problema3/main.cpp
@@ -3,6 +3,9 @@
 #include <cstdlib>
 #include <vector>
 #include <limits>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
 
 #define MAX 1000
 
@@ -10,15 +13,70 @@ std::vector<std::vector<std::pair<int, int>>> matrix;
 
 int nVertex, apCount, dfs[MAX], low[MAX], parent[MAX], dfsTimer;
 bool isAP[MAX];
-int **dist;
+int **adjency;
+
+std::vector< std::pair<long double, std::pair<int, int> > > edges;
+int vSet[MAX], vRank[MAX];
+
+int parent2[MAX]; 
+  
+// Find set of vertex i 
+int find(int i) 
+{ 
+    while (parent2[i] != i) 
+        i = parent2[i]; 
+    return i; 
+} 
+  
+// Does union of i and j. It returns 
+// false if i and j are already in same 
+// set. 
+void union1(int i, int j) 
+{ 
+    int a = find(i); 
+    int b = find(j); 
+    parent2[a] = b; 
+} 
+  
+
+// Finds MST using Kruskal's algorithm 
+int kruskalMST() 
+{ 
+    int mincost = 0; // Cost of min MST. 
+  
+    // Initialize sets of disjoint sets. 
+    for (int i = 1; i <= nVertex; i++) 
+        parent2[i] = i; 
+  
+    // Include minimum weight edges one by one 
+    int edge_count = 0; 
+    while (edge_count < nVertex - 1) { 
+        int min = std::numeric_limits<int>::max() / 2, a = -1, b = -1; 
+        for (int i = 1; i <= nVertex; i++) { 
+            for (int j = 1; j <= nVertex; j++) { 
+                if (find(i) != find(j) && adjency[i][j] < min) {
+                    min = adjency[i][j]; 
+                    a = i; 
+                    b = j; 
+                } 
+            } 
+        } 
+  
+        union1(a, b); 
+        
+        edge_count++;
+        mincost += min; 
+    } 
+    return mincost; 
+} 
 
 void freeArray()
 {
     for (int i = 1; i <= nVertex; i++)
     {
-        free(dist[i]);
+        free(adjency[i]);
     }
-    free(dist);
+    free(adjency);
     //printf("UNALLOC INPUT %d\n", nVertex);
 }
 
@@ -28,18 +86,20 @@ void resetData()
     matrix.resize(nVertex + 1);
     apCount = 0;
     dfsTimer = 0;
-
     for (int i = 1; i <= nVertex; i++)
     {
         dfs[i] = -1;
         isAP[i] = false;
     }
 
-    dist = (int **)malloc((nVertex + 1) * sizeof(int *));
+    adjency = (int **)malloc((nVertex + 1) * sizeof(int *));
     for (int i = 1; i <= nVertex; i++)
     {
-        dist[i] = (int *)malloc((nVertex + 1) * sizeof(int));
+        adjency[i] = (int *)malloc((nVertex + 1) * sizeof(int));
     }
+
+    edges.clear();
+    edges.resize(nVertex + 1);
 }
 
 void articulationPoints(int v)
@@ -92,6 +152,16 @@ int articulationCount()
     return apCount;
 }
 
+bool decrease(const std::pair<long double, std::pair<int, int> > &a, const std::pair<long double, std::pair<int, int> > &b) {
+	if(a.first == b.first) {
+		if(a.second.first == b.second.first) {
+        	return (a.second.second < b.second.second);
+		}
+		else return (a.second.first < b.second.first);
+	}
+	else return (a.first > b.first);
+}
+
 void readInput()
 {
     std::string str;
@@ -129,8 +199,7 @@ void readInput()
         matrix[vertexA].push_back(std::make_pair(vertexB, cost));
         matrix[vertexB].push_back(std::make_pair(vertexA, cost));
 
-        dist[vertexA][vertexB] = cost;
-
+        adjency[vertexA][vertexB] = cost;
         //printf("%d %d cost: %d\n", vertexA, vertexB, cost);
         //printf("VertexA %d first: %d second: %d\n", vertexA, matrix[vertexA][vertexB].first, matrix[vertexA][vertexB].second);
     }
@@ -144,14 +213,14 @@ void floydWarshall()
     {
         for (j = 1; j <= nVertex; j++)
         {
-            if (dist[i][j])
-                dist[i][j] = dist[i][j];
+            if (adjency[i][j])
+                adjency[i][j] = adjency[i][j];
             else if (i == j)
             {
-                dist[i][j] = 0;
+                adjency[i][j] = 0;
             }
             else
-                dist[i][j] = std::numeric_limits<int>::max() / 2;
+                adjency[i][j] = std::numeric_limits<int>::max() / 2;
         }
     }
 
@@ -161,7 +230,7 @@ void floydWarshall()
         {
             for (j = 1; j <= nVertex; j++)
             {
-                dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
+                adjency[i][j] = std::min(adjency[i][j], adjency[i][k] + adjency[k][j]);
             }
         }
     }
@@ -186,17 +255,10 @@ int main()
 
         //Number of servers
         int serverNumber = articulationCount();
-
+        
         if (serverNumber == 0)
         {
             printf("no server\n");
-            //   for (int i = 1; i <= nVertex; i++)
-            // {
-            //     for (int j = 1; j <= nVertex; j++)
-            //     {
-            //         printf("%d %d cost: %d\n", i, j, dist[i][j]);
-            //     }
-            // }
         }
         else if (serverNumber == 1)
         {
@@ -208,19 +270,15 @@ int main()
                 printf("Vertex %d: AP: %d \n", i, isAP[i]);
             }
 #endif
-            //  for (int i = 1; i <= nVertex; i++)
-            // {
-            //     for (int j = 1; j <= nVertex; j++)
-            //     {
-            //         printf("%d %d cost: %d\n", i, j, dist[i][j]);
-            //     }
-            // }
         }
         else
         {
-
-            floydWarshall();
+            
             int linkServersCost = 0;
+            int treeCost = 0;
+
+            treeCost = kruskalMST();
+            floydWarshall();
 
             for (int i = 1; i <= nVertex; i++)
             {
@@ -230,25 +288,27 @@ int main()
                     {
                         if (isAP[j])
                         {
-                            if (dist[i][j] != std::numeric_limits<int>::max() / 2)
+                            if (adjency[i][j] != std::numeric_limits<int>::max() / 2)
                             {
-                                linkServersCost += dist[i][j];
+                                linkServersCost += adjency[i][j];
                             }
                         }
                     }
                 }
             }
-            //  for (int i = 1; i <= nVertex; i++)
-            // {
-            //     for (int j = 1; j <= nVertex; j++)
-            //     {
-            //         if(dist[i][j] != 0 && dist[i][j] != std::numeric_limits<int>::max() / 2){
-            //         printf("%d %d cost: %d\n", i, j, dist[i][j]);
-            //         }
-            //     }
-            // }
+             for (int i = 1; i <= nVertex; i++)
+            {
+                for (int j = 1; j <= nVertex; j++)
+                {
+                    if(adjency[i][j] != 0 && adjency[i][j] != std::numeric_limits<int>::max() / 2){
+                    //printf("%d %d cost: %d\n", i, j, adjency[i][j]);
+                    }
+                }
+            }
 
-            printf("%d %d %d\n", serverNumber, linkServersCost, 0);
+            
+
+            printf("%d %d %d\n", serverNumber, linkServersCost, treeCost);
         }
         freeArray();
     }
