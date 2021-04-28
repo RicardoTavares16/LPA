edd14df4ae9e6d64af7c7662d93e105532ecd87f
Ricardo Tavares <ricardotavares16@gmail.com>
2021-04-28 12:16:13 +0100

    Trabalho2 initial commit

---
+++ Trabalho2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    int testCases;
+    int n, h, H;
+    std::cin >> testCases;
+
+    for (int i = 0; i < testCases; i++)
+    {
+        std::cin >> n;
+        std::cin >> h;
+        std::cin >> H;
+        //std::cout << n << " " << h << " " << H << std::endl;
+    }
+}
