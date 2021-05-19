a812c54ea62bb25eddb89f3d07a83dd824fe64ce
Ricardo Tavares <ricardotavares16@gmail.com>
2021-05-19 22:50:39 +0100

    All answers done

--- Trabalho3/main.cpp
+++ Trabalho3/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <algorithm>
 
 int t;
 std::vector<std::vector<std::pair<int, int>>> adjMatrix;
@@ -8,6 +9,82 @@ std::vector<int> dfs;
 std::vector<int> low;
 std::vector<int> vertexStack;
 std::vector<std::vector<int>> stronglyConnectedComponents;
+std::vector<std::pair<long double, std::pair<int, int>>> edges;
+int set[1000], rank[1000];
+
+void makeSet(int vCount)
+{
+    for (int i = 1; i <= vCount; i++)
+    {
+        set[i] = i;
+        rank[i] = 0;
+    }
+}
+
+int find(int a)
+{
+    if (set[a] != a)
+    {
+        set[a] = find(set[a]);
+    }
+    return set[a];
+}
+
+void link(int a, int b)
+{
+    if (rank[a] > rank[b])
+    {
+        set[b] = a;
+    }
+    else
+    {
+        set[a] = b;
+        if (rank[a] == rank[b])
+        {
+            rank[b]++;
+        }
+    }
+}
+
+void unionSet(int a, int b)
+{
+    link(find(a), find(b));
+}
+
+long double kruskal(int vCount)
+{
+    long double w, weight = 0;
+    int a, b;
+    makeSet(vCount);
+    while (!edges.empty())
+    {
+        a = edges.back().second.first;
+        b = edges.back().second.second;
+        w = edges.back().first;
+        if (find(a) != find(b))
+        {
+            weight += w;
+            unionSet(a, b);
+        }
+        edges.pop_back();
+    }
+    return weight;
+}
+
+bool decrease(const std::pair<long double, std::pair<int, int>> &a, const std::pair<long double, std::pair<int, int>> &b)
+{
+    if (a.first == b.first)
+    {
+        if (a.second.first == b.second.first)
+        {
+            return (a.second.second < b.second.second);
+        }
+        else
+            return (a.second.first < b.second.first);
+    }
+    else
+        return (a.first > b.first);
+}
 
 void tarjan(int v)
 {
@@ -64,10 +141,11 @@ void countStronglyConnectedComponents(int vCount)
     }
 }
 
-void printAnswers(int questions)
+void printAnswers(int questions, int vCount)
 {
     int answers[4] = {0};
     int maxPOICount = 0;
+    int maxKruskal = 0;
 
     for (int i = 0; i < (int)stronglyConnectedComponents.size(); i++)
     {
@@ -84,10 +162,45 @@ void printAnswers(int questions)
                     maxPOICount = stronglyConnectedComponents[i].size();
                 }
             }
+
+            if (questions > 2)
+            {
+                for (int j = 0; j < (int)stronglyConnectedComponents[i].size(); j++)
+                {
+                    int a = stronglyConnectedComponents[i][j]; // vertex a
+
+                    for (int k = 0; k < (int)adjMatrix[stronglyConnectedComponents[i][j]].size(); k++)
+                    {
+                        for (int h = 0; h < (int)stronglyConnectedComponents[i].size(); h++)
+                        {
+                            if (adjMatrix[stronglyConnectedComponents[i][j]][k].first == stronglyConnectedComponents[i][h])
+                            {
+                                int b = adjMatrix[stronglyConnectedComponents[i][j]][k].first;  // vertex b
+                                int c = adjMatrix[stronglyConnectedComponents[i][j]][k].second; // cost
+
+                                edges.push_back(std::make_pair(c, std::make_pair(a, b)));
+                            }
+                        }
+                    }
+                }
+
+                std::sort(edges.begin(), edges.end(), decrease);
+                int answer3tmp = (int)kruskal(vCount);
+
+                if (answer3tmp > maxKruskal)
+                {
+                    maxKruskal = answer3tmp;
+                }
+
+                if(questions > 3){
+                    answers[3] += answer3tmp;
+                }
+            }
         }
     }
 
     answers[1] = maxPOICount;
+    answers[2] = maxKruskal;
 
     for (int i = 0; i < questions; i++)
     {
@@ -127,7 +240,7 @@ int main()
 
         countStronglyConnectedComponents(poiCount);
 
-        printAnswers(questions);
+        printAnswers(questions, poiCount);
     }
     return 0;
 }
